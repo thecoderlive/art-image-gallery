76f93341f09c43c3eb7838f5e2a0f15450a890db
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-07 09:08:38 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,30 @@
+export const item = {
+    "id": "dc3de999-0d6b-40f5-82f9-cc5fa6897d55",
+    "home_feed": [
+        {
+            "id": "d14e7a06-d309-4d1a-a07e-859d6d87e2ed",
+            "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png",
+            "art_caption": "four"
+        },
+        {
+            "id": "f753f03d-9cea-4415-83a4-6ee3e827b513",
+            "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png",
+            "art_caption": "I"
+        },
+        {
+            "id": "2baa3da3-a49f-407e-80b0-c7f3eb035c55",
+            "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/6.png",
+            "art_caption": "main"
+        },
+        {
+            "id": "8b26078c-8103-492d-a13f-53383b4911f9",
+            "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png",
+            "art_caption": "trade"
+        },
+        {
+            "id": "4a1ace7d-5420-4eaa-ae57-5f53a9cee235",
+            "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/77.png",
+            "art_caption": "fall"
+        }
+    ]
+}
